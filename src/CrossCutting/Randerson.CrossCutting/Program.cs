4e582c85b934b9eca9c141fbeb8033d2aa876f47
randerson.reis <randerson.reis@CPX-R027V9K5C5U>
2021-08-13 13:23:46 -0300

    Add project files.

---
+++ src/CrossCutting/Randerson.CrossCutting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Randerson.CrossCutting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Domain/Randerson.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Randerson.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
